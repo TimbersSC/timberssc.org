12fc84c454e5c111f057994f54a3fecb30bc6b37
Michael Podsiadly <1161101+The-Podsiadly@users.noreply.github.com>
2023-08-25 11:04:03 -0600

    feat: fix nav and begin work on featured

--- frontend/landing/src/components/layout.tsx
+++ frontend/landing/src/components/layout.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
-import { MantineProvider, AppShell, Box } from "@mantine/core";
+import { MantineProvider, AppShell, Box } from '@mantine/core'
 
-import "../styles/app.scss";
+import '../styles/app.scss'
 
-import { NavigationComponent } from "./nav";
-import { FooterComponent } from "./footer";
+import { NavigationComponent } from './nav'
+import { FooterComponent } from './footer'
 
 export default function Layout({ children }) {
   return (
@@ -14,12 +14,12 @@ export default function Layout({ children }) {
       withNormalizeCSS
       theme={{
         // Override any other properties from default theme
-        fontFamily: "Noto Sans, sans serif",
+        fontFamily: 'Noto Sans, sans-serif',
         // spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
-        defaultGradient: { from: "#315235", to: "#cb9c66", deg: 20 },
+        defaultGradient: { from: '#315235', to: '#cb9c66', deg: 20 },
         colors: {
-          gold: ["#cb9c66"],
-          evergreen: ["#315235"],
+          gold: ['#cb9c66'],
+          evergreen: ['#315235'],
         },
       }}
     >
@@ -28,10 +28,10 @@ export default function Layout({ children }) {
         // padding="md"
         header={<NavigationComponent />}
         footer={<FooterComponent />}
-        padding={"none"}
+        padding={'none'}
       >
-        <Box>{children}</Box>
+        <Box sx={{ position: 'relative', zIndex: 1 }}>{children}</Box>
       </AppShell>
     </MantineProvider>
-  );
+  )
 }
